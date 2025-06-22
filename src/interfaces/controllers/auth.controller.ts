c642419287133b11b0ae4b00197b6cf17f28797b
sasamile <nspes2020@gmail.com>
2025-06-21 23:13:39 -0500

    feat:Update secure production

--- src/interfaces/controllers/auth.controller.ts
+++ src/interfaces/controllers/auth.controller.ts
@@ -74,13 +74,13 @@ export class AuthController {
     const result = await this.loginUseCase.execute(request);
     res.cookie('refreshToken', result.refreshToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
+      secure: true,
       sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     res.cookie('accessToken', result.accessToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
+      secure: true,
       sameSite: 'none',
       maxAge: 35 * 60 * 1000,
     });
@@ -113,14 +113,14 @@ export class AuthController {
     // Opcional: puedes volver a refrescar las cookies si quieres
     res.cookie('refreshToken', result.refreshToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
+      secure: true,
       sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 días
     });
 
     res.cookie('accessToken', result.accessToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
+      secure: true,
       sameSite: 'none',
       maxAge: 35 * 60 * 1000, // 35 min
     });
