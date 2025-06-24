4129df6c5af6cad2a7b07a742b8d6bd52492e9da
sasamile <nspes2020@gmail.com>
2025-06-23 23:55:51 -0500

    add origin main

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
       'http://localhost:3000',
       'http://127.0.0.1:3000',
       'http://192.168.56.1:3000',
+      'https://client-software-mirro.vercel.app/',
       'https://www.eduadminsoft.shop',
       'https://eduadminsoft.shop',
     ],
